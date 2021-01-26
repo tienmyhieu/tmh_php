a02c29424c2b235497f990b3e3b5977d83747068
Tiền mỹ hiệu <tienmyhieu@gmail.com>
2021-01-25 20:36:54 -0500

    initalize 172

--- src/adapters/ReferenceEmperors.php
+++ src/adapters/ReferenceEmperors.php
@@ -15,6 +15,8 @@ class ReferenceEmperors extends Marshal
     {
         $referenceEmperors = [];
         $previousReferenceId = 0;
+        $patterns = ['|\.|', '|\,|', "|\'|", '|\s+|'];
+        $replacements = ['', '', '', '_'];
         foreach ($this->referenceEmperors() as $referenceEmperor) {
             $referenceYear = $this->references[$referenceEmperor['reference_uuid']]['year'];
             $year = 0 < strlen($referenceYear) ? $referenceYear : date('Y');
@@ -24,10 +26,11 @@ class ReferenceEmperors extends Marshal
             }
             $emperors[$referenceEmperor['emperor_uuid']] = $this->emperors[$referenceEmperor['emperor_uuid']];
             $emperors[$referenceEmperor['emperor_uuid']]['count'] = $referenceEmperor['count'];
-            $emperors[$referenceEmperor['emperor_uuid']]['href'] = $this->references[$referenceEmperor['reference_uuid']]['acronym'] . '#' . $emperors[$referenceEmperor['emperor_uuid']]['href'];
+            $href =  preg_replace($patterns, $replacements, $this->references[$referenceEmperor['reference_uuid']]['title']);
+            $emperors[$referenceEmperor['emperor_uuid']]['href'] = $href . '#' . $emperors[$referenceEmperor['emperor_uuid']]['href'];
             $referenceEmperors[$referenceEmperor['reference_uuid']] = [
                 'acronym' => $this->references[$referenceEmperor['reference_uuid']]['acronym'],
-                'href' => $this->references[$referenceEmperor['reference_uuid']]['acronym'],
+                'href' => $href,
                 'year' => $year,
                 'title' => $this->references[$referenceEmperor['reference_uuid']]['title'],
                 'emperors' => $emperors,
